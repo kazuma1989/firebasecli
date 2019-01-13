32c1eb3808eae0840725f49db64c9fe1fb915fe1
kazuma1989 <funifuni.1204@gmail.com>
2019-01-13 22:28:23 +0900

    feature: Add help and flags.

--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/kazuma1989/firebasecli
 
 require (
 	firebase.google.com/go v3.5.0+incompatible
+	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
 	github.com/googleapis/gax-go v2.0.2+incompatible // indirect
 	google.golang.org/api v0.1.0
 )
--- run.go
+++ run.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"os"
+
+	docopt "github.com/docopt/docopt-go"
 )
 
 // Run starts the app.
@@ -13,34 +15,64 @@ func Run(args ...string) error {
 		return err
 	}
 
+	opts, err := docopt.ParseArgs(`
+Manipulate Cloud Firestore.
+
+Usage:
+  firebasecli db list
+  firebasecli db export COLLECTIONS... [-f]
+
+Options:
+  list  List the all collections.
+
+Options:
+  export COLLECTIONS  Export collections as JSON.
+  -f, --format        Output as formatted JSON.
+`, args, "")
+	if err != nil {
+		return err
+	}
+
+	var arg struct {
+		Db          bool
+		List        bool
+		Export      bool
+		Collections []string
+		Format      bool
+	}
+	if err := opts.Bind(&arg); err != nil {
+		return err
+	}
+
 	p := &Printer{
 		Stdout: os.Stdout,
 		Stderr: os.Stderr,
 	}
-
 	ctx := context.Background()
-	switch {
-	case len(args) <= 1:
 
-	case args[0] == "db" && args[1] == "list":
+	switch {
+	case arg.List:
 		collections, err := app.DbList(ctx)
 		if err != nil {
 			return err
 		}
 		p.Println(collections)
 
-	case args[0] == "db" && args[1] == "export":
-		data, err := app.DbExport(ctx, []string{"meta"})
+	case arg.Export:
+		data, err := app.DbExport(ctx, arg.Collections)
 		if err != nil {
 			return err
 		}
-		jsonData, err := json.Marshal(data)
+		var jsonData []byte
+		if arg.Format {
+			jsonData, err = json.MarshalIndent(data, "", "  ")
+		} else {
+			jsonData, err = json.Marshal(data)
+		}
 		if err != nil {
 			return err
 		}
 		p.Println(string(jsonData))
-
-	default:
 	}
 
 	return nil
